7dd0092f50a292f6134ed67a571713c0bfb99396
Roland Heintze <rheintze@linux.com>
2013-10-27 15:28:17 -0400

    Reverted django-registration changes.

--- game_guides/accounts/urls.py
+++ game_guides/accounts/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, url
-# from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('accounts.views',
-    # url(r'^$', 'accounts', name='accounts-dashboard'),
-    url(r'^$', include('registration.urls')),
+    url(r'^$', 'accounts', name='accounts-dashboard'),
 
 
     # Django Defaults
 
-    # url(r'^login/$', login, {'template_name': 'accounts/login.html'}),
-    # url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}),
+    url(r'^login/$', login, {'template_name': 'accounts/login.html'}),
+    url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}),
 )
