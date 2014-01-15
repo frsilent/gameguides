04a01eae403d7eefcf841455924a279f9ecb6352
Roland <rheintze@linux.com>
2014-01-15 16:55:28 -0500

    Oops

--- game_guides/game_guides/settings/demo.py
+++ game_guides/game_guides/settings/demo.py
@@ -8,7 +8,7 @@ from django.core.exceptions import ImproperlyConfigured
 DEBUG = True
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'gameguides',
         'USER': 'opensorcerer',
         'PASSWORD': 'password',
