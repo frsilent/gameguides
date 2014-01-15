4dc599457edd1f4d73204ff9496a902f0b12460c
Roland <rheintze@linux.com>
2014-01-14 21:22:55 -0500

    Added temporary settings file for demoing

--- game_guides/django.wsgi
+++ game_guides/django.wsgi
@@ -5,6 +5,7 @@ sys.path.append('/var/www/gameguides/game_guides')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'game_guides.settings.production'
 os.environ['PYTHON_EGG_CACHE'] = '/var/www/gameguides/cache'
+os.environ['DEFAULT_CACHE_ALIAS'] = '/var/www/gameguides/cache'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
---
+++ game_guides/game_guides/settings/demo.py
@@ -0,0 +1,25 @@
+"""Demo settings and globals."""
+
+from os import environ
+from base import *
+from django.core.exceptions import ImproperlyConfigured
+
+
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2'
+        'NAME': 'gameguides',
+        'USER': 'opensorcerer',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+    }
+}
+
+TEMPLATE_DEBUG = DEBUG
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
