8652c4a59dda48cf42f97821b68165ad46242149
Roland <rheintze@linux.com>
2014-06-03 17:21:29 -0400

    Fleshed out angular-style filtering.

--- game_guides/static/js/guide-list.js
+++ game_guides/static/js/guide-list.js
@@ -2,6 +2,7 @@ app = angular.module('guides.list', []);
 
 app.controller('GuideListController', [
     '$scope', '$http', function($scope, $http) {
+        $scope.filters = { };
         $scope.guides = [];
         return $http.get('/api/guides').then(function(result) {
             return angular.forEach(result.data, function(item) {
