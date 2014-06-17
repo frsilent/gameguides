8eb12aa72b596aa7bc4ea6849279e25baa801fa6
Roland <rheintze@linux.com>
2014-06-16 21:47:21 -0400

    Added preliminary pagination

--- game_guides/static/js/guide-list.js
+++ game_guides/static/js/guide-list.js
@@ -10,6 +10,21 @@ app.controller('GuideListController', ['$scope', '$http', '$filter', function($s
         });
     });
 
+    // Pagination
+    $scope.currentPage = 0;
+    $scope.pageSize = 2;
+    $scope.numberOfPage = function() {
+        return Math.ceil($scope.guides.length/$scope.pagesSize);
+    }
+    $scope.paginate = function() {
+        return function(input, start) {
+            start = +start;
+            return input.slice(start);
+        }
+    }
+
+
+
     $scope.toggleCategory = function(category_id) {
         // Adds or removes a category id from $scope.categories
         var exists = $filter('filter')($scope.categories, category_id, true);
@@ -32,3 +47,9 @@ app.controller('GuideListController', ['$scope', '$http', '$filter', function($s
     };
 
 }]);
+
+// TODO: Use this to fix pagination & filtering:
+// http://jsfiddle.net/2ZzZB/56/
+// http://stackoverflow.com/questions/11581209/angularjs-pagination-on-a-list-using-ng-repeater
+// http://jsfiddle.net/SAWsA/11/
+
