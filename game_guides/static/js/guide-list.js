0a065831b86aed1041e2267a5806a01e5159a66b
Roland <rheintze@linux.com>
2014-06-23 19:11:44 -0400

    Fixed bug in guide list filtering

--- game_guides/static/js/guide-list.js
+++ game_guides/static/js/guide-list.js
@@ -3,8 +3,8 @@ var app = angular.module('guides.list', [])
 app.controller('GuideListController', ['$scope', '$http', '$filter', function($scope, $http, $filter) {
     $scope.guides = [];
     $scope.categories = [];
-    $http.get('/api/guides').then(function(result) {
-        // Fetches all guides from /api/guides endpoint
+
+    $http.get('/api/guides').then(function(result) {        // Fetches all guides from /api/guides endpoint
         angular.forEach(result.data, function(item) {
             $scope.guides.push(item);
         });
@@ -14,20 +14,20 @@ app.controller('GuideListController', ['$scope', '$http', '$filter', function($s
     $scope.currentPage = 0;
     $scope.pageSize = 6;
 
-    $scope.toggleCategory = function(category_id) {
-        // Adds or removes a category id from $scope.categories
+
+    $scope.toggleCategory = function(category_id) {         // Adds to category array or removes it if it already exists
         var exists = $filter('filter')($scope.categories, category_id, true);
         if(exists.length) {
-            $scope.categories.pop(category_id);
+            var existing_index = $scope.categories.indexOf(category_id);
+            $scope.categories.splice(existing_index, 1);
         } else {
             $scope.categories.push(category_id);
         };
         $scope.currentPage = 0;
     };
 
-    $scope.category_filter = function(guide) {
-        // filters out guides that do not have any categories in the current $scope.categories
-        if(!$scope.categories.length) { return true; }
+    $scope.category_filter = function(guide) {              // Filter for all guide objects; Returns all if categories is empty or returns guides only with matching categories
+        if($scope.categories.length == 0) { return true; }
         for(var i = 0, len = $scope.categories.length; i < len; i++) {
             if(guide.categories.indexOf($scope.categories[i]) == -1) {
                 return false;
